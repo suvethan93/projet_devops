3d645f3e46758fc6104760415b051f6b6856f6d1
Suvethan <suve.viji@gmail.com>
2026-03-10 21:00:04 +0000

    Enrichissement du contenu de l'API et ajout de la route /info

--- src/app.py
+++ src/app.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
 
-app= FastAPI()
+app = FastAPI(
+    title="API DevOps",
+    description="Projet - Conteneurisation et Réseau",
+    version="1.0.0"
+)
 
 @app.get("/")
-
 def read_root():
-    return{"Status": "Succès:","Message": "Projet DevOps opérationnel"}
+    return {
+        "Status": "Succès",
+        "Projet": "SDN DevOps",
+        "Binôme": "Suvethan & Mattéo",
+        "Infrastructure": "Docker sur VM Ubuntu (UTM)",
+        "Message": "L'API est parfaitement opérationnelle !"
+    }
+
+@app.get("/info")
+def get_info():
+    return {
+        "Python_Version": "3.13",
+        "Framework": "FastAPI",
+        "Container_User": "app (non-root)",
+        "Documentation": "Disponible sur /docs"
+    }
